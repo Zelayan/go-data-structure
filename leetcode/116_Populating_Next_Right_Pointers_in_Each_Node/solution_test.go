855d70804ba11d910440ed5da1ed34951f108d2c
zeze <chenzezeya@gmail.com>
2022-07-18 22:43:23 +0800

    fix:test

---
+++ leetcode/116_Populating_Next_Right_Pointers_in_Each_Node/solution_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_connect(t *testing.T) {
+	type args struct {
+		root *Node
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Node
+	}{}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := connect(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("connect() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
