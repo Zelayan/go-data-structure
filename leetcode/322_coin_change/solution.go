cc0116f25c54cfbc8607003a4f120c791a717c24
zeze <chenzezeya@gmail.com>
2022-07-24 23:54:22 +0800

    solution for leetcode 322

---
+++ leetcode/322_coin_change/solution.go
@@ -0,0 +1,50 @@
+package _22_coin_change
+
+import "math"
+
+func coinChange(coins []int, amount int) int {
+	if len(coins) == 0 {
+		return -1
+	}
+	if amount == 0 {
+		return -1
+	}
+	dp := make([]int, amount+1)
+	for i := 0; i < amount; i++ {
+		dp[i] = amount + 1
+		for j := range coins {
+			if i-coins[j] < 0 {
+				continue
+			}
+			dp[i] = min(dp[i], dp[i-coins[j]]+1)
+		}
+	}
+
+	return dp[amount]
+}
+func coinChange2(coins []int, amount int) int {
+	if amount < 1 && len(coins) < 1 {
+		return -1
+	}
+	memo := make([]int, amount+1)
+	for i := 1; i <= amount; i++ {
+		memo[i] = math.MaxInt32
+		for _, c := range coins {
+			if i >= c && memo[i] > memo[i-c]+1 {
+				memo[i] = memo[i-c] + 1
+			}
+		}
+	}
+	if memo[amount] == math.MaxInt32 {
+		return -1
+	}
+	return memo[amount]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
